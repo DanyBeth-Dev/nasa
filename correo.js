e6b7e87fcad0ad007db4f69f3e0ac91a6ef41cc7
Daniela Hernandez <danielabelen.hg90@gmail.com>
2022-05-23 15:34:41 -0400

    OAuth approach with false values

--- correo.js
+++ correo.js
@@ -1,21 +1,6 @@
 const res = require("express/lib/response");
 const nodemailer = require("nodemailer");
 
-//simple approach
-
-/* let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.AUTH_EMAIL,
-      pass: process.env.AUTH_PASS,
-    },
-    pool: true,
-    secure: false,
-    tls: {
-      rejectUnauthorized: false,
-    }
-  }); */
-
 //OAUTH approach
 
 let transporter = nodemailer.createTransport({
@@ -26,12 +11,17 @@ let transporter = nodemailer.createTransport({
     clientID: process.env.AUTH_CLIENT_ID,
     clientSecret: process.env.AUTH_CLIENT_SECRET,
     refreshToken: process.env.AUTH_REFRESH_TOKEN
+  },
+  pool: true,
+  secure: false,
+  tls: {
+    rejectUnauthorized: false,
   }
 });
 
 //testing nodemailer success
 transporter.verify((error, success) => {
-  if(error) {
+  if (error) {
     console.log(error)
   } else {
     console.log("Ready for messages");
@@ -40,13 +30,13 @@ transporter.verify((error, success) => {
 })
 
 const enviarCorreo = async (email, nombre) => {
-    let mailOptions = {
-        from: process.env.AUTH_EMAIL,
-        to: [email],
-        subject: `¡Saludos desde la NASA!`,
-        html: `<h3> ¡Hola!, ${nombre}! <br> La nasa te da las gracias por subir tu foto a nuestro sistema y colaborar con las investigaciones extraterrestres. </h3>`,
-    };
-    await transporter.sendMail(mailOptions)
+  let mailOptions = {
+    from: process.env.AUTH_EMAIL,
+    to: [email],
+    subject: `¡Saludos desde la NASA!`,
+    html: `<h3> ¡Hola!, ${nombre}! <br> La nasa te da las gracias por subir tu foto a nuestro sistema y colaborar con las investigaciones extraterrestres. </h3>`,
+  };
+  await transporter.sendMail(mailOptions)
 };
 
 module.exports = enviarCorreo;
